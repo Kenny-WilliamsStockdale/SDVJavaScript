65b1950aa9c0363c05b477c4567b0183e5b2cbe2
Kenny <Kenny-williamsStockd@live.nmit.ac.nz>
2021-03-09 10:55:47 +1300

    added code to js file

--- src/week-three.js
+++ src/week-three.js
@@ -1,1 +1,22 @@
-console.log ('kenny')
+//TODO JavaScript Variables
+
+// Declare a variable first name and last name and assign a value. A string in this case.
+var firstName = 'Kenny'
+var lastName = 'Stockdale'
+
+//Changing value of lastName variable example
+//lastName = 'Stuff'
+
+// Print it individually 
+// console.log(firstName)
+// console.log(lastName)
+
+// print it with another variable + some ES6 
+let fullName = firstName + ' ' + lastName
+fullName = `My name is ${firstName} ${lastName}` //ES6 $ sign
+
+// more variables with assigned values
+let string = 'Hello SDV503 class'
+
+console.log (fullName)
+console.log (string)
