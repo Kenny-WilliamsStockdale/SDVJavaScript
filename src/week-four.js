15cfff4de84a57dcfbbbde246ede5649e51987f8
Kenny <Kenny-williamsStockd@live.nmit.ac.nz>
2021-03-15 12:49:03 +1300

    added code to js file

---
+++ src/week-four.js
@@ -0,0 +1,14 @@
+// Values & Variables
+
+let country = 'New Zealand'
+let city = 'Nelson' //data type string
+let postalCode = 7010 // data type number
+
+console.log(country);
+console.log(city);
+console.log(postalCode);
+
+let isIsland = true // boolean
+let language  // 
+console.log(isIsland)
+console.log(language)
