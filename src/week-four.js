dada63af2839e990a11214c18f52037d32369e7e
Kenny <Kenny-williamsStockd@live.nmit.ac.nz>
2021-03-15 14:35:52 +1300

    more examples in js file

--- src/week-four.js
+++ src/week-four.js
@@ -8,7 +8,53 @@ console.log(country);
 console.log(city);
 console.log(postalCode);
 
-let isIsland = true // boolean
+let isIsland = true // data type boolean
 let language  // 
 console.log(isIsland)
 console.log(language)
+
+language = 'English'
+console.log(language)
+
+let number = 10 
+console.log(number)
+
+console.log(number / 2)
+console.log(number > 6)
+console.log(number < 2)
+
+const address = country + " " + city + " " + postalCode
+console.log(address)
+
+let string = `My address is ${country}, ${city}, ${postalCode}.`
+console.log(string)
+
+let x = 10;
+
+    if(x < 0) {         // if and else statement example
+        console.log('Ali')
+} else {
+    console.log('its false')
+}
+
+x = 'Ali'
+if (x = 'Ali' || typeof 'string') {
+    console.log('this is true')
+} else {
+    console.log('Error')
+}
+
+// country = 'Iraq'
+// if (typeof country === 'string') {
+//     console.log('True')
+// } else {
+//     console.log('False')
+// }
+
+// let example = 100;
+
+// if( example < 50) {
+//     console.log('tis good')
+// } else {
+//     console.log('no good delete')
+// }
