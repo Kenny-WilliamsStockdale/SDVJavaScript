e11bd750779f17577ba5430e29351a0c764fecfc
Kenny <Kenny-williamsStockd@live.nmit.ac.nz>
2021-03-16 10:02:20 +1300

    added more example code to js

--- src/week-four.js
+++ src/week-four.js
@@ -38,20 +38,52 @@ let x = 10;
 }
 
 x = 'Ali'
-if (x = 'Ali' || typeof 'string') {
+if (x === 'Ali' || typeof 'string') {
     console.log('this is true')
 } else {
     console.log('Error')
 }
 
+x = '1234567'
+console.log(typeof x)  // playing around with more Variables and learning how to convert data types and save then with other Variables
+let y = Number(x)
+//console.log()
+console.log(typeof y)
+
+const a = 1+1    // using multiple variables to add values to one and other just print out not save. 
+console.log(a)
+
+const b = a + 50
+console.log(b)
+
+let z           // making undefined datatype 
+console.log(z)
+
+const information = {  // Example of using object template 
+    student : 'Ali',
+    country : 'Iraq',
+    city : 'Nelson',
+    postalCode : 7010
+}
+console.log(information)
+
+let myInfo = {             // Example of object using different keyword 
+    name : 'Kenny Stockdale',
+    age : '27',
+    study : 'Nmit',
+    postalCode : 7010,
+    country : 'New Zealand'
+}
+console.log(myInfo)
+
 // country = 'Iraq'
-// if (typeof country === 'string') {
+// if (typeof country === 'string') {   //Generic code instead of hardcoded
 //     console.log('True')
 // } else {
 //     console.log('False')
 // }
 
-// let example = 100;
+// let example = 100;   // another if or else statement example 
 
 // if( example < 50) {
 //     console.log('tis good')
