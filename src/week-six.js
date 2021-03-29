0a957b9143ff845cdaf9faa7825c6911f3b41a62
Kenny <Kenny-williamsStockd@live.nmit.ac.nz>
2021-03-30 10:48:27 +1300

    added obj + func examples

--- src/week-six.js
+++ src/week-six.js
@@ -36,3 +36,66 @@ numbers.push(4)
 numbers.push(5)
 
 console.log(numbers) //to interpret change is a change in memory address. Clarify memory or if change of value
+
+let arr = [1, 2, 3, 4, 5]
+console.log(arr.length)
+
+const student = {  //object example
+    name: "Bob",
+    year: "one",
+    class: "SDV503"
+}
+
+console.log(student.name)
+student.name = "Matt"  // changing object name
+console.log(student.name)
+
+const newObj = {} 
+
+const info = {      // another object
+    firstName: 'John',
+    lastName: 'Smith',
+    dateOfBirth: '1st DEC 2000',
+    friends: ['Bob', 'Tom'] 
+}
+
+console.log(info.friends[0]) //Getting friend array index value 0 and print
+
+const example = {
+    nestedObj: {
+        name: 'Alex' //nested object example
+    }
+}
+console.log(example)
+
+const anotherObj = new Object() //
+console.log(anotherObj)
+
+const propsName ='firstName'
+
+const objOne = {        // object with array
+    [propsName.toUpperCase()]: 'Alex'
+}
+
+console.log(objOne)
+
+const objTwo = {           //function and an object AKA a method example
+    firstName: 'Alex',
+    lastName: 'Ali',
+    printFullName() { //Function here
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+console.log(objTwo.printFullName())
+console.log(objTwo['firstName'])
+
+console.log(objTwo.hasOwnProperty('lastName')) // confirming if object has lastName property in object 
+console.log(objTwo.hasOwnProperty('address')) // confirming if object has address property in object 
+
+console.log(delete objTwo.firstName) //deleting the first name property
+
+console.log(objTwo.hasOwnProperty('firstName')) // checking if the property is there or not after delete command
+
+const newProps = delete objTwo.firstName // creating a variable to delete it 
+console.log(newProps)
